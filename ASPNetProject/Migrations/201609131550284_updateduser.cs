d432821854b8af821d31907b92a08399dd9bd43b
kchipp <khipp2@gmail.com>
2016-09-13 11:28:44 -0500

    updated user model

---
+++ ASPNetProject/Migrations/201609131550284_updateduser.cs
@@ -0,0 +1,22 @@
+namespace ASPNetProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateduser : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "UserName", c => c.String(nullable: false));
+            AlterColumn("dbo.Users", "FirstName", c => c.String(nullable: false));
+            AlterColumn("dbo.Users", "LastName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "LastName", c => c.String());
+            AlterColumn("dbo.Users", "FirstName", c => c.String());
+            AlterColumn("dbo.Users", "UserName", c => c.String());
+        }
+    }
+}
